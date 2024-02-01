7e1e943fbf512582d91ecd4fa31038c1801c5c05
Dasiy233 <419826967@qq.com>
2024-02-01 16:18:38 +0800

    firsy

--- Array/Binary search.cpp
+++ Array/Binary search.cpp
@@ -1,1 +1,22 @@
- git remote add origin https://github.com/Dasiy233/c-.git
+#include<iostream>
+using namespace std;
+
+class Solution{
+public:
+    int search (int nums[],int target){
+        for(int i=0;i<sizeof(nums);i++){
+            if( nums[i] == target )
+                return i;
+        }
+            return -1;
+
+    }
+};
+
+int main(){
+    int nums[3]={1,2,3};
+        Solution a;
+        a.search(nums,3);
+        system("pause");
+        return 0 ;
+}
